524eb3434d29970e63b258f58bfd14b4e994af62
Devon Maywald <dmaywald@tamu.edu>
2024-09-10 10:43:17 -0500

    complete SteepestDescent and NewtonsMethod functions

--- NewtonsMethod.R
+++ NewtonsMethod.R
@@ -7,20 +7,20 @@
 # x0 - initial starting point
 # nIter - positive integer, number of iterations
 
-NewtonsMethod <- function(f, fprime, fdoubleprime, x0, nIter){
-  
+NewtonsMethod <- function(f, fprime, fdoubleprime, x0, nIter) {
   # Initialize storage for iterations and function values
   fvec = rep(f(x0), nIter + 1)
   xvec = rep(x0, nIter + 1)
   
   # Perform Newton's update for nIter iterations
-  for (i in 1:nIter){
+  for (i in 1:nIter) {
     # At each iteration, update current value of x and save the new function value
     # [ToDo] fill in
     # Newton's method update
-
+    xvec[i + 1] = xvec[i] - (fprime(xvec[i]) / fdoubleprime(xvec[i]))
+    
     # Function value
-
+    fvec[i + 1] = f(xvec[i + 1])
   }
   
   # Return the vector of x values, as well as the vector of function values across iterations, including the starting point (both length nIter + 1)
--- SteepestDescent.R
+++ SteepestDescent.R
@@ -7,21 +7,21 @@
 # alpha - positive number, step size
 # nIter - positive integer, number of iterations
 
-SteepestDescent <- function(f, fprime, x0, alpha, nIter){
-  
+SteepestDescent <- function(f, fprime, x0, alpha, nIter) {
   # Initialize storage for iterations and function values
   fvec = rep(f(x0), nIter + 1) # nIter + 1 so that the starting point is saved in addition
   xvec = rep(x0, nIter + 1) # nIter + 1 so that the starting point is saved in addition
   
   # Perform steepest descent update for nIter iterations
-  for (i in 1:nIter){
+  for (i in 1:nIter) {
     # At each iteration, update current value of x
     # and save the new function value
     # [ToDo] fill in
     # Steepest descent update
-
+    xvec[i + 1] = xvec[i] - alpha * fprime(xvec[i])
+    
     # Function value
-
+    fvec[i + 1] = f(xvec[i + 1])
   }
   
   # Return the vector of x values, as well as the vector of function values across iterations, including the starting point (both length nIter + 1)
